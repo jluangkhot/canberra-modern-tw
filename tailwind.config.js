0f890d10b91daa446761ad75946685852b204b31
PhuongUyenLe <161270074+PhuongUyenLe@users.noreply.github.com>
2024-04-11 23:00:58 +1000

    1234

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,7 @@ module.exports = {
 				primary: '#3490dc', // Add your custom primary color
 				secondary: '#6574cd', // Add your custom secondary color
 				debug: '#FFCA28', // Just so you can see how this is powerful
+				cream: '#FFF9E0'
 				// Add more custom colors as needed
 			},
 			fontFamily: {
